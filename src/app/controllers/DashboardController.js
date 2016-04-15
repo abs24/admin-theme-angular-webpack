5485038daa3cfd184cd77152aacc8b0104aa0e43
abs24 <4.ks.abhi@gmail.com>
2016-04-15 19:18:12 +0530

    15-4-2016

--- src/app/controllers/DashboardController.js
+++ src/app/controllers/DashboardController.js
@@ -1,6 +1,17 @@
 export default class DashboardController {
   constructor() {
-    
+    this.recentFacebookSearch = [
+		{productTitle:"Logitech B100 USB (Wired) Optical Mouse (Black)",store:"paytm",pageName:"",pageImg:"https://assetscdn.paytm.com/images/catalog/product/C/CO/COMLOGITECH-B10JIPL29324EA98432F/323x575/70/3.png",timestamp:"1458646635"},
+		{productTitle:"Logitech B100 USB (Wired) Optical Mouse (Black)",store:"paytm",pageName:"",pageImg:"https://assetscdn.paytm.com/images/catalog/product/C/CO/COMLOGITECH-B10JIPL29324EA98432F/323x575/70/3.png",timestamp:"1458646635"},
+	
+	
+	
+	];
+	this.productURL="";
+  }
+  addProduct(){
+	  
+	  
   }
 
   
